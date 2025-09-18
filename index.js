44d643e840412ed704186eaa74d0fffad9191b3f
YoussefAbdalnaby <86475811+YoussefAbdalnaby@users.noreply.github.com>
2025-09-18 18:11:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express=require("express")
 const app=express()
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require("path");
+
 const database=require("./database/database.js")
 const bookRoute=require("./routes/bookRoute.js")
 const userRoute=require("./routes/userRoute.js")
@@ -14,7 +19,50 @@ app.use(express.json())
 
 database.connectToDatabase()
 
-
+app.get("/chat", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+// Socket.io connection handling
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+    
+    // Broadcast to all clients that a user joined
+    socket.broadcast.emit('user joined', {
+        id: socket.id,
+        message: 'A user has joined the chat'
+    });
+    
+    // Listen for chat messages
+    socket.on('chat message', (data) => {
+        console.log('Message received:', data);
+        
+        // Broadcast message to all clients
+        io.emit('chat message', {
+            id: socket.id,
+            message: data.message,
+            timestamp: new Date().toLocaleTimeString()
+        });
+    });
+    
+    // Listen for typing events
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', {
+            id: socket.id,
+            isTyping: data.isTyping
+        });
+    });
+    
+    // Handle user disconnect
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        socket.broadcast.emit('user left', {
+            id: socket.id,
+            message: 'A user has left the chat'
+        });
+    });
+    
+   
+});
 
 app.use("/api/books",bookRoute)
 app.use("/api/users",userRoute)
@@ -27,6 +75,6 @@ app.use("/api/moives",moiveRoute)
 
 
 
-app.listen(3000,()=>{   
+server.listen(3000,()=>{   
     console.log("listening on port 3000")
 }   )
